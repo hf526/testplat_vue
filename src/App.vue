6a42a677f6c211b50b9243ea45978b99c02301bd
hf526 <x13155916315@qq.com>
2019-11-17 22:36:01 +0800

    Signed-off-by: hf526 <x13155916315@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <img src="./assets/logo.png">
     <div>
       <p>
+        666666
         If Element is successfully added to this project, you'll see an
         <code v-text="'<el-button>'"></code>
         below
